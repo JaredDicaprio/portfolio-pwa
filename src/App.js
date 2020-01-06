a02d7d6007957b3ef4791f443e9c4f8de2a54211
Uddesh <uddesh@Uddeshs-MacBook-Air.local>
2020-01-06 19:32:18 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router, Link } from '@reach/router';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const NavBar = () => (
+  <div className="navbar">
+    <h3>Task Manager</h3>
+    <Link to="/">Current Tasks</Link>
+    <Link to="/completed">Completed Tasks</Link>
+  </div>
+);
+
+const Template = (props) => (
+  <div>
+    <NavBar />
+    <p className="page-info">
+      {props.title}:
+    </p>
+    <ul className={props.status}>
+        <li>Task 1</li>
+        <li>Task 2</li>
+        <li>Task 3</li>
+    </ul>
+  </div>
+);
+
+const CurrentTasks = () => (
+  <Template title="Current Tasks" status="Current"/>
+);
+
+const CompletedTasks = () => (
+  <Template title="Completed Tasks" status="Completed"/>
+);
+
+const App = () => {
+    return (
+      <div>
+        <Router>
+          <CurrentTasks path="/" />
+          <CompletedTasks path="/completed" />
+        </Router>
+      </div>
+    );
 }
 
 export default App;
