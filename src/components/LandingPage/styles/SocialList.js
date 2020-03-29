a33dc2a4094ec5ab96ef76b7f917b08e4ccec876
Uddesh Jain <jainuddesh35@gmail.com>
2020-03-29 16:55:57 +0530

    Added svg.

--- src/components/LandingPage/styles/SocialList.js
+++ src/components/LandingPage/styles/SocialList.js
@@ -1,34 +1,31 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
 const SocialList = styled.ul`
-    position: relative;
-    list-style: none;
-    margin: 40px 0 0 0;
-    padding: 0;
-    text-decoration: none;
-    & > li {
-        display: inline;
-        margin: 0 30px 0 0;
-        &:last-child {
-            margin: 0;
-        };
-        & > a {
-            font-size: 20px;
-            text-decoration: none;
-            color: ${props => props.theme.fontColor};
-            &:hover {
-                color: #4edac4;
-            };
-            & > i {
-                font-size: 20px;
-                transform: scale(1);
-                transition: all 0.3s ease;
-                &:hover {
-                    transform: scale(1.3);
-                };
-            };
-        };
-    };
+  position: relative;
+  list-style: none;
+  margin: 40px 0 0 0;
+  padding: 0;
+  text-decoration: none;
+  & > li {
+    display: inline;
+    margin: 0 30px 0 0;
+    &:last-child {
+      margin: 0;
+    }
+    & > a {
+      font-size: 20px;
+      text-decoration: none;
+      & > svg {
+        fill: #939396;
+        transform: scale(1);
+        transition: all 0.3s ease;
+        &:hover {
+          transform: scale(1.3);
+          fill: #31c4ad;
+        }
+      }
+    }
+  }
 `;
 
 export default SocialList;
